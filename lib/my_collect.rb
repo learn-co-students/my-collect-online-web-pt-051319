1389b6522f5c53b2ae9440801c053c5e3e5c84be
Annie Truex <artruex@gmail.com>
2019-05-18 04:33:49 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+#langauages = ['ruby', 'javascript', 'python', 'objective-c']
+#my_collect(langauages) do |langauage|
+#  language.upcase
+#end
+def my_collect(collection)
+ i = 0
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
+  end
+  new_collection
+end
